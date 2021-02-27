29194ab2e29034406b434ecfd2610984c33e78c7
kasper084 <kasper084@gmail.com>
2021-02-28 00:28:54 +0200

    moved call of msg into inputInt method

--- src/main/scala/one/ReadIntTask.scala
+++ src/main/scala/one/ReadIntTask.scala
@@ -8,31 +8,29 @@ object ReadIntTask extends App {
   doSimpleMath()
 
   def doSimpleMath(): Unit = {
-    //xMinusY()
-    xAndYAndZ()
+    xMinusY()
+    //xAndYAndZ()
   }
 
   def msg(): Unit = {
     println("Enter int")
   }
 
-  def inputInt(): Int = readInt()
+  def inputInt(): Int = {
+    msg()
+    readInt()
+  }
 
   def xMinusY(): Unit = {
-    msg()
     val x = inputInt()
-    msg()
     val y = inputInt()
     val result = x - y
     println(s"$x-$y=$result")
   }
 
   def xAndYAndZ(): Unit = {
-    msg()
     val x = inputInt()
-    msg()
     val y = inputInt()
-    msg()
     val z = inputInt()
     val result = x * y * z
     println(s"$x*$y*$z=$result")
